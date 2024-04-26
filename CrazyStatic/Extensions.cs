14ea4b21de849a5f1a8a9dfc3a1cadf7564aaf7b
Ramón Martínez <ramartinez@aidacanarias.com>
2024-04-26 08:20:47 +0200

    first commit

---
+++ CrazyStatic/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace CrazyStatic;
+internal static class Extensions
+{
+
+    public static SomeSingleton AddMessageToSomeSingletonData(
+        this List<string> someData,
+        string message)
+    {
+        var someSingleton = SomeSingleton.Instance();
+
+        lock (someSingleton)
+        {
+            someData.Add(message);
+        }
+
+        return someSingleton;
+    }
+    public static SomeSingleton AddMessageToSomeSingle(
+        this SomeSingleton someSingleton,
+        string message)
+    {
+
+        lock (someSingleton)
+        {
+            someSingleton.SomeData.Add(message);
+        }
+
+        return someSingleton;
+    }
+
+}
---
+++ CrazyStatic/SomeSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrazyStatic;
+
+public class SomeSingleton
+{
+    private static SomeSingleton _instance;
+
+    public List<string> SomeData { get; }
+
+    private SomeSingleton()
+    {
+        SomeData = new();
+    }
+
+    public static SomeSingleton Instance() =>
+        _instance ??= new SomeSingleton();
+
+    public override string ToString()
+    {
+        return String.Join(", ", SomeData.ToArray());
+    }
+}
